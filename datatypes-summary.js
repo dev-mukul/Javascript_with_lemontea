5a2944617f582fd5a2219329304dc313246c9f16
Mukul Kumar <145434427+dev-mukul@users.noreply.github.com>
2023-09-30 10:57:53 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,40 @@
+// # Primitive 
+
+//  7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+// Js is 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log( id === anotherId);
+
+
+// const bigNumber = 34675865369379n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Obejcts, Functions
+
+const heros = ["shaktiman", "doga"]
+//  let myObj {
+//     name: "Mukul"
+//     age: 22
+
+// }
+
+const myFunction = function(){
+    console.log("Hello wolrd");
+
+}
+
+console.log(typeof myFunction)

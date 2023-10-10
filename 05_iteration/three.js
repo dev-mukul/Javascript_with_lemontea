febe1c725f4052141c9aa64f0612068bbcc49736
Mukul Kumar <145434427+dev-mukul@users.noreply.github.com>
2023-10-10 13:17:47 +0000

    for in loop

---
+++ 05_iteration/three.js
@@ -0,0 +1,41 @@
+//Higher Order Array Loops
+
+// for of 
+
+// ["" , "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+const greeting = "Hello world!"
+for(const num of greeting){
+    // console.log(`Each char is ${num}`);
+}
+
+// Maps
+
+c
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+    
+}
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+// for(const [key, value] of myObject){
+//     console.log(key, ':-', value);
+// }
+
+

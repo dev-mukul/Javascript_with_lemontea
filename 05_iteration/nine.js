66d65a7ad23465ba1fb08828012effcb738094e8
Mukul Kumar <145434427+dev-mukul@users.noreply.github.com>
2023-10-10 15:37:21 +0000

    reduce methods

---
+++ 05_iteration/nine.js
@@ -0,0 +1,37 @@
+// <----- Reduce Method ------> 
+
+const myNums = [1, 2, 4]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// },0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        iteamName: "js course",
+        price: 2999
+    },
+    {
+        iteamName: "java",
+        price: 3999
+    },
+    {
+        iteamName: "mobile dev course",
+        price: 4999
+    },
+    {
+        iteamName: "AI course",
+        price: 6599
+    },
+    {
+        iteamName: "data science course",
+        price: 2999
+    },
+]
+
+ const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+ console.log(priceToPay);

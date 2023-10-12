3f84252c82071ab6f123a4c906c0071920e2d50e
Mukul Kumar <145434427+dev-mukul@users.noreply.github.com>
2023-10-12 21:09:40 +0000

    objects in new keywords

---
+++ 10_classes_and_oop/Objects.js
@@ -0,0 +1,33 @@
+// <------- Prototype ------> 
+
+function multipleBy5(num){
+    this.num = num
+    return num*5
+
+}
+multipleBy5.power = 2
+
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype);
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+
+
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+createUser.prototype.printMe = function(){
+    console.log(`score is ${this.score}`);
+}
+
+const chai = new createUser("coffee", 199)
+const tea =  createUser("tea", 25)
+
+chai.printMe()
+
+

8321e2bb9b9fc7b82767eb81c6a4596b3a8669a8
Mukul Kumar <145434427+dev-mukul@users.noreply.github.com>
2023-10-13 19:28:34 +0000

    class constructor

---
+++ 10_classes_and_oop/myClasses.js
@@ -0,0 +1,46 @@
+// ES6
+
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password
+
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const coffee = new User("coffee", "coffee@gmail.com", "123")
+
+console.log(coffee.encryptPassword());
+console.log(coffee.changeUsername());
+
+
+
+// //behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+
+}
+
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function () {
+    return `${this.password}abc`
+}
+
+const coldcoffee = new User("coldcoffee", "coffee@gmail.com", "123")
+console.log(coldcoffee.encryptPassword());
+console.log(coldcoffee.changeUsername());
+
